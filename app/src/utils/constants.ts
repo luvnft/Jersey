4facc5d78af7b8a57b8346a4e51d083cbab2c56f
The Wizard of Hahz <info@luvnft.com>
2025-02-22 06:38:19 -0500

    updated

--- app/src/utils/constants.ts
+++ app/src/utils/constants.ts
@@ -1,5 +1,5 @@
 import { Connection, PublicKey } from '@solana/web3.js';
 
-export const SOLANA_NETWORK = "http://127.0.0.1:8899";
+export const SOLANA_NETWORK = "https://devnet.helius-rpc.com/?api-key=40734dc1-6a29-40dd-ac22-1a3e98aa9edb";
 export const connection = new Connection(SOLANA_NETWORK, 'confirmed');
 export const PROGRAM_ID = new PublicKey("9h4jHLwgmwdRJETCYFx28Q85xarwzaqUwuimMvkfVLeX"); 
